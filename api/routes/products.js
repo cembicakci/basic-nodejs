aa12fb8b2bbf41da0a262fb9c1c7ffbd84d26435
Cem Bıçakcı <cmbicakci@gmail.com>
2024-11-02 16:24:34 +0300

    mongodb connect

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose")
+
+const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
 	res.status(200).json({
@@ -8,31 +11,44 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-	
-	const product = {
+	const product = new Product({
+		_id: new mongoose.Types.ObjectId(),
 		name: req.body.name,
 		price: req.body.price
-	}
-
-	res.status(201).json({
-		message: "POST products is OK",
-		createdProduct: product
 	});
+
+	product.save()
+		.then(result => {
+			console.log(result)
+
+			res.status(201).json({
+				message: "POST products is OK",
+				createdProduct: product
+			})
+		})
+		.catch(err => {
+			console.log(err)
+			res.status(500).json({ error: err })
+		})
 });
 
 router.get("/:productId", (req, res, next) => {
 	const { productId } = req.params
-	if (productId === "special") {
-		res.status(200).json({
-			message: "Special ID is OK",
-			id: productId
+	Product.findById(productId)
+		.exec()
+		.then(doc => {
+			console.log("From DB", doc)
+
+			if (doc) {
+				res.status(200).json(doc)
+			} else {
+				res.status(404).json({ message: "No valid product for provided id" })
+			}
 		})
-	} else {
-		res.status(200).json({
-			message: "Wrong Id",
-			id: productId
+		.catch(err => {
+			console.log(err)
+			res.status(500).json({ error: err })
 		})
-	}
 })
 
 router.patch("/:productId", (req, res, next) => {
--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose")
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+mongoose.connect(`mongodb+srv://cmbicakci:@node-basic.rzzmn.mongodb.net/?retryWrites=true&w=majority&appName=node-basic`)
 
 // Body Parser
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
+
 
 // CORS
 app.use((req, res, next) => {
