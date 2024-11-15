e2b361aa54d0c40d44f2c71a18709f4ef0f163d5
Cem Bıçakcı <cmbicakci@gmail.com>
2024-11-16 00:14:17 +0300

    orders connected db

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,48 +1,88 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const Order = require("../models/order");
+const Product = require("../models/product");
 
 router.get('/', (req, res, next) => {
-	res.status(200).json({
-		message: "GET /orders is OK"
-	})
+	Order.find()
+		.select('product quantity _id')
+		.exec()
+		.then(docs => {
+			res.status(200).json({
+				count: docs.length,
+				orders: docs
+			})
+		})
+		.catch(err => {
+			res.status(500).json({
+				error: err
+			})
+		})
 })
 
 router.post('/', (req, res, next) => {
-	const order = new Order({
-		_id: new mongoose.Types.ObjectId(),
-		quantity: req.body.quantity,
-		product: req.body.productId
-	})
-	console.log(order)
+	Product.findById(req.body.productId)
+		.then(product => {
+			if (!product) {
+				return res.status(404).json({
+					message: "Product not found"
+				})
+			}
+			const order = new Order({
+				_id: new mongoose.Types.ObjectId(),
+				quantity: req.body.quantity,
+				product: req.body.productId
+			})
 
-	order.save()
+			return order.save()
+		})
 		.then((result) => {
-			console.log(result)
 			res.status(201).json(result)
 		})
 		.catch((err) => {
 			console.log(err)
 			res.status(500).json({
-				error: err.message
+				error: err
 			})
 		})
 })
 
 router.get('/:orderId', (req, res, next) => {
-	res.status(200).json({
-		message: "GET order_detail is OK",
-		orderId: req.params.orderId
-	})
+	Order.findById(req.params.orderId)
+		.select('product quantity _id')
+		.exec()
+		.then(order => {
+			if (!order) {
+				return res.status(404).json({
+					message: "Order not found"
+				})
+			}
+			res.status(200).json({
+				order: order
+			})
+		})
+		.catch(err => {
+			res.status(500).json({
+				error: err
+			})
+		})
 })
 
 router.delete('/:orderId', (req, res, next) => {
-	res.status(200).json({
-		message: "Order deleted",
-		orderId: req.params.orderId
-	})
+	Order.deleteOne({ _id: req.params.orderId })
+		.exec()
+		.then(() => {
+			res.status(200).json({
+				message: "Order deleted"
+			})
+		})
+		.catch(err => {
+			res.status(500).json({
+				error: err
+			})
+		})
 })
 
 module.exports = router;
